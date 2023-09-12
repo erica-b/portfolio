486cc1ba058ee2b536596b384d43d59d22acd646
Erica Bitting <ericabitting@gmail.com>
2023-09-11 21:14:06 -0400

    added Index and Portfolio routes

---
+++ erica-portfolio/src/Components/Index.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import '../App.css'
+
+export default function Index() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Erica Bitting</h1>
+      <p className="read-the-docs">
+        Welcome to my portfolio. 
+      </p>
+      <form action="/portfolio">
+      <button>See more</button>
+      </form>
+    </>
+  )
+}
+
+// export default Index
